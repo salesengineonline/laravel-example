f08073fa6ebcf48918f2b4f23b9a00dafd3ffb06
Pavel Egorov <pavel.egorov@salesengineonline.com>
2016-09-15 01:09:06 +0100

    fixed isuues

--- config/app.php
+++ config/app.php
@@ -80,7 +80,7 @@ return [
 
 	'key' => env('APP_KEY', 'eaAJlaAlABGDmSOIOoBKZBuwZnGdjYMn'),
 
-	'cipher' => 'AES-256-CBC',
+	'cipher' => 'rijndael-128',
 
 	/*
 	|--------------------------------------------------------------------------
